cfd351cf41833b234281f842336599eb709c69e5
Dinura Ransika <ransikadinura@gmail.com>
2026-02-19 23:58:23 +0530

    feat: implement routing structure with Routes and nested Route components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Profile from './pages/Profile'
+import FoodLog from './pages/FoodLog'
+import ActivityLog from './pages/ActivityLog'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="food" element={<FoodLog />} />
+          <Route path="activity" element={<ActivityLog />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
